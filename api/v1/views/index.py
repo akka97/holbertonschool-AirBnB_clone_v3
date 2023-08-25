c2de254a56a3c770186cdcde191a703a9ddedc02
akka97 <akapellari@yahoo.com>
2023-08-25 08:43:54 +0200

    tasks

---
+++ api/v1/views/index.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""First route to display a json object"""
+from api.v1.views import app_views
+from flask import jsonify
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+
+
+tables = {"amenities": Amenity, "cities": City,
+          "places": Place, "reviews": Review, "states": State, "users": User}
+
+
+@app_views.route('/status')
+def status():
+    '''status route'''
+    return jsonify(status="OK")
+
+
+@app_views.route('/stats')
+def stats():
+    ''''status fun'''
+    counts = {}
+    for key, val in tables.items():
+        counts[key] = storage.count(val)
+    return jsonify(**counts)
